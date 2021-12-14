7900809932c1d16f0e6885643c43e5750e430bed
user <4tiamr24@solent.ac.uk>
2021-12-14 23:48:53 +0000

    Initial commit

---
+++ Basic/if_else.py
@@ -0,0 +1,11 @@
+# Ask user to enter activity
+print("Please enter the activity to be performed")
+activity = input()
+
+# Determine if the activity is Calculate
+if activity == "calculate":
+   print("Performing Calculations...")
+else:
+    print("Performing Activity...")
+
+print("Activity Completed")
