eab4968c133b4ac2a3138bc511b782d7713a4901
user <4tiamr24@solent.ac.uk>
2021-12-14 22:48:40 +0000

    Initial commit

---
+++ Basic/IF.py
@@ -0,0 +1,8 @@
+# Ask user for the type of book
+print("What type of book is this?")
+book_type = input()
+# Determine the book if it's an adventure book
+if book_type == "Adventure":
+    print("I like adventure books")
+# Display Message
+print("Finished reading book")
